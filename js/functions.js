0ebf996e2f16e0039e3dc80c33b686052a33b710
Zyz <zuzz001zu@gmail.com>
2026-03-09 15:01:53 +0100

    corrected

--- js/functions.js
+++ js/functions.js
@@ -8,7 +8,7 @@ lineLength('проверяемаястрока', 20);
 lineLength('проверяемаястр', 18);
 lineLength('провер', 10);
 
-function polidrome (theLine) {
+function checkPalidrome (theLine) {
   const string = theLine.replaceAll(' ', '');
   const str = string.toLowerCase();
   let str1 = '';
@@ -18,7 +18,7 @@ function polidrome (theLine) {
   return str === str1;
 }
 
-polidrome('топот');
-polidrome('ДовОд');
-polidrome('Кекс');
-polidrome('Лёша на полке клопа нашёл ');
+checkPalidrome('топот');
+checkPalidrome('ДовОд');
+checkPalidrome('Кекс');
+checkPalidrome('Лёша на полке клопа нашёл ');
