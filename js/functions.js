ac24115cd1b3c3b45d3dc004e5f7ad0ab395ed7c
Zyz <zuzz001zu@gmail.com>
2026-03-10 08:57:24 +0100

    corrected

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,19 @@
 function lineLength (theLine, maxLinelength) {
-  if (theLine.length <= maxLinelength) {
-    return true;
-  }
-  return false;
+  return theLine.length <= maxLinelength;
 }
+
 lineLength('проверяемаястрока', 20);
 lineLength('проверяемаястр', 18);
 lineLength('провер', 10);
 
 function checkPalidrome (theLine) {
-  const string = theLine.replaceAll(' ', '');
-  const str = string.toLowerCase();
-  let str1 = '';
+  const str = theLine.replaceAll(' ', '').toLowerCase();
+  let obtainedString = '';
+
   for (let i = str.length - 1; i >= 0; i--) {
-    str1 += str.at(i);
+    obtainedString += str.at(i);
   }
-  return str === str1;
+  return str === obtainedString;
 }
 
 checkPalidrome('топот');
